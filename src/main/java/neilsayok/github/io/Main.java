2fa4b22569b2e173bb1b030cc1b1516d5f96d909
NeilMajumder <sdmsdm1998@gmail.com>
2020-08-18 13:30:30 +0530

    Initial commit

---
+++ src/main/java/neilsayok/github/io/Main.java
@@ -0,0 +1,40 @@
+package neilsayok.github.io;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    static String driverPath = "F:\\Java\\Projects\\QueueApp\\SeleniumTest1\\src\\chromedriver.exe";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",driverPath);
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://queueapp-64877.firebaseapp.com/business");
+
+        WebElement bName = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/input"));
+        WebElement bAddress = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/input"));
+        WebElement openTime = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div[1]/input"));
+        WebElement closeTime = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div[2]/input"));
+
+        bName.sendKeys("My Buiseness");
+        bAddress.sendKeys("Some Address");
+        openTime.sendKeys("value","02:20");
+        closeTime.sendKeys("value","15:25");
+        //driver.findElement(By.id("open")).setAttribute("value", "your value");
+
+
+
+
+    }
+
+
+
+
+}
